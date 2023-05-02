6bb4157f98b261fd4522c9953c79d632d952c3b4
kawsarhamidi <kawsarhamidi77@gmail.com>
2023-05-02 19:58:13 +0600

    add to chef data agein

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [chef, setChef] = useState([]);
+
+    useEffect( ()=>{
+        fetch('http://localhost:5000/chef')
+        .then(res=> res.json())
+        .then(data=> setChef(data))
+        .catch(error=> console.error(error))
+    },[])
     return (
-        <div>
-            left nav
+        <div className=''>
+            <h4>All chef details</h4>
+
+            {
+                chef.map(chefs => <div className="mt-4" key={chefs.id}>
+                    <Card style={{ width: '18rem' }} >
+                <Card.Img variant="top" src={chefs.picture} />
+                <Card.Body>
+                  <Card.Title>{chefs.name}</Card.Title>
+                  <Card.Text>
+                  Years Of Experience: {chefs.yearsOfExperience}
+                    </Card.Text>
+                  <Card.Text>
+                  Number Of Recipes: {chefs.numberOfRecipes}
+                    </Card.Text>
+                  <Card.Text>
+                  Likes: {chefs.likes}
+                    </Card.Text>
+                  <Link to={`/chef/${chef.id}`}><Button variant="primary">Go to details</Button></Link> 
+                </Card.Body>
+              </Card>
+                </div>
+              )
+            }
         </div>
     );
 };
